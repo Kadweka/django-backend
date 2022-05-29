4f0e9167580ac4a6162ec6dbc78527334e6ca503
Kadweka <husseinkatana17@gmail.com>
2022-05-29 18:31:56 +0300

    update the views

--- expenses/settings.py
+++ expenses/settings.py
@@ -67,6 +67,25 @@ TEMPLATES = [
         },
     },
 ]
+TEMPLATES = [
+    {
+        # Template backend to be used, For example Jinja
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        # Directories for templates
+        'DIRS': ['templates'],
+        'APP_DIRS': True,
+ 
+        # options to configure
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'expenses.wsgi.application'
 
